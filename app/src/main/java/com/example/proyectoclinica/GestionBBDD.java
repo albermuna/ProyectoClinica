cd61422786e713286c82f8a1c430978c72ffa0da
albermuna <albermuna@users.noreply.github.com>
2023-04-17 15:47:07 +0200

    Base

--- app/src/main/java/com/example/proyectoclinica/GestionBBDD.java
+++ app/src/main/java/com/example/proyectoclinica/GestionBBDD.java
@@ -1,10 +1,15 @@
 package com.example.proyectoclinica;
 
 import android.content.Context;
+import com.example.proyectoclinica.clasespojo.Centros;
+import com.example.proyectoclinica.clasespojo.Citas;
 import com.example.proyectoclinica.clasespojo.Doctores;
+import com.example.proyectoclinica.clasespojo.Pacientes;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 
 public class GestionBBDD {
@@ -19,13 +24,47 @@ public class GestionBBDD {
     }
 
     public boolean introducirDoctor (Doctores doc) {
-        Doctores d = new Doctores();
-        d.setDni(doc.getDni());
-        d.setApellidos(doc.getApellidos());
-        d.setNombre(doc.getNombre());
-        d.setEspecialidad(doc.getEspecialidad());
-        d.setCitasDoctor(doc.getCitasDoctor());
-        databaseReference.child("Doctores").child(d.getDni()).setValue(d);
+        HashMap<String, Object> doctor = new HashMap<>();
+        doctor.put("dni", doc.getDni());
+        doctor.put("nombre", doc.getNombre());
+        doctor.put("apellidos", doc.getApellidos());
+        doctor.put("especialidad", doc.getEspecialidad());
+        doctor.put("citas", doc.getCitasDoctor());
+        databaseReference.child("doctores").child(doc.getDni()).setValue(doctor);
+        return true;
+    }
+
+    public boolean introducirPaciente (Pacientes pac) {
+        HashMap<String, Object> paciente = new HashMap<>();
+        paciente.put("dni", pac.getDni());
+        paciente.put("nombre", pac.getNombre());
+        paciente.put("apellidos", pac.getApellidos());
+        paciente.put("edad", pac.getEdad());
+        paciente.put("citas", pac.getCitasPaciente());
+        databaseReference.child("pacientes").child(pac.getDni()).setValue(paciente);
+        return true;
+    }
+
+    public boolean introducirCentro (Centros cen) {
+        HashMap<String, Object> centro = new HashMap<>();
+        centro.put("uid", cen.getUid());
+        centro.put("nombre", cen.getNombre());
+        centro.put("direccion", cen.getDireccion());
+        centro.put("doctores", cen.getDoctoresCentro());
+        centro.put("pacientes", cen.getPacientesCentro());
+        centro.put("citas", cen.getCitasCentro());
+        databaseReference.child("centros").child(cen.getUid()).setValue(centro);
+        return true;
+    }
+
+    public boolean introducirCita (Citas cit) {
+        HashMap<String, Object> cita = new HashMap<>();
+        cita.put("uid", cit.getUid());
+        cita.put("fecha y hora", cit.getFechaHora());
+        cita.put("doctor", cit.getDoctor().getDni());
+        cita.put("paciente", cit.getPaciente().getDni());
+        cita.put("centro", cit.getCentro().getUid());
+        databaseReference.child("citas").child(cit.getUid()).setValue(cita);
         return true;
     }
 
--- app/src/main/java/com/example/proyectoclinica/pantallas/InicioSesion.java
+++ app/src/main/java/com/example/proyectoclinica/pantallas/InicioSesion.java
@@ -15,8 +15,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.*;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import org.jetbrains.annotations.NotNull;
 
 public class InicioSesion extends AppCompatActivity {
     TextView nuevoUsuario;
@@ -38,59 +40,56 @@ public class InicioSesion extends AppCompatActivity {
             public void onClick(View view) {
                 String dniUsuario = dni.getText().toString();
                 String contraseñaUsuario = contraseña.getText().toString();
+                validarPaciente(dniUsuario, contraseñaUsuario);
+                validarDoctor(dniUsuario, contraseñaUsuario);
 
-                db.collection("pacientes")
-                        .whereEqualTo("dni", dniUsuario)
-                        .whereEqualTo("contraseña", contraseñaUsuario)
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    if (task.getResult().size() > 0) {
-                                        // Las credenciales son correctas, iniciar sesión como paciente
-                                        // ...
-                                    } else {
-                                        // Las credenciales son incorrectas, mostrar un mensaje de error
-                                        // ...
-                                    }
-                                } else {
-                                    // Error al comprobar las credenciales, mostrar un mensaje de error
-                                    // ...
-                                }
-                            }
-                        });
 
-                db.collection("doctores")
-                        .whereEqualTo("dni", dniUsuario)
-                        .whereEqualTo("contraseña", contraseñaUsuario)
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    if (task.getResult().size() > 0) {
-                                        // Las credenciales son correctas, iniciar sesión como doctor
-                                        // ...
-                                    } else {
-                                        // Las credenciales son incorrectas, mostrar un mensaje de error
-                                        // ...
-                                    }
-                                } else {
-                                    // Error al comprobar las credenciales, mostrar un mensaje de error
-                                    // ...
-                                }
-                            }
-                        });
             }
         });
 
     }
 
+    private void validarDoctor(String dniUsuario, String contraseñaUsuario) {
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("doctores");
+        Query query = dbRef.orderByKey().equalTo(dniUsuario);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String contraseña = snapshot.child("contraseña").getValue(String.class);
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
 
+            }
+        });
+    }
 
-
+    private void validarPaciente(String dniUsuario, String contraseñaUsuario) {
+        db.collection("pacientes")
+                .whereEqualTo("dni", dniUsuario)
+                .whereEqualTo("contraseña", contraseñaUsuario)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (task.getResult().size() > 0) {
+                                // Las credenciales son correctas, iniciar sesión como paciente
+                                // ...
+                            } else {
+                                // Las credenciales son incorrectas, mostrar un mensaje de error
+                                // ...
+                            }
+                        } else {
+                            // Error al comprobar las credenciales, mostrar un mensaje de error
+                            // ...
+                        }
+                    }
+                });
+    }
 
 
     public void createUser(View view) {
